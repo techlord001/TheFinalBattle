106be18f962f5a91e426dffbc22a2a0b5084170a
techlord001 <45688942+techlord001@users.noreply.github.com>
2023-01-28 13:20:37 +0000

    Add Character & Party Classes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,48 @@ namespace TheFinalBattle
     {
         static void Main(string[] args)
         {
-            
+
+        }
+
+        public class Party
+        {
+            public List<Character> Characters { get; private set; }
+
+            public Party()
+            {
+                Characters = new List<Character>();
+            }
+
+            public void Add(params Character[] characters)
+            {
+                foreach (Character character in characters)
+                {
+                    Characters.Add(character);
+                }
+            }
+        }
+
+        public class Skeleton : Character
+        {
+            public Skeleton() : base("SKELETON")
+            {
+            }
+        }
+
+        public abstract class Character
+        {
+            public string Name { get; set; }
+
+            public Character(string name)
+            {
+                Name = name;
+            }
+
+            public void Action()
+            {
+                Console.WriteLine($"{Name} did NOTHING");
+                Thread.Sleep(500);
+            }
         }
     }
 }
