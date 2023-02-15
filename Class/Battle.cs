82daf97f17cfd420c9f171b359e2bceb965130bf
Tech Lord <45688942+techlord001@users.noreply.github.com>
2023-02-15 18:03:03 +0000

    Add Battle Status Console Output
    Because:
    - is needed to provide info of enemy status to make more informed action choice

--- Class/Battle.cs
+++ Class/Battle.cs
@@ -22,6 +22,39 @@
         }
     }
 
+    private void Status(CharacterBase player)
+    {
+        Message.WriteLine(("").PadRight(30, '=') + " BATTLE " + ("").PadRight(30, '='), ConsoleColor.White);
+
+        foreach (CharacterBase character in Heroes.Characters)
+        {
+            if (character == player)
+            {
+                Message.WriteLine($"{character.Name, -20}* ( {character.CurrentHP}/{character.MaxHP} )", ConsoleColor.Blue);
+            }
+            else
+            {
+                Message.WriteLine($"{character.Name, -20} ( {character.CurrentHP}/{character.MaxHP} )", ConsoleColor.Blue);
+            }
+        }
+
+        Message.WriteLine(("").PadRight(32, '-') + " VS " + ("").PadRight(32, '-'), ConsoleColor.White);
+
+        foreach (CharacterBase character in Monsters.Characters)
+        {
+            if (character == player)
+            {
+                Message.WriteLine($"{character.Name, 58}* ( {character.CurrentHP}/{character.MaxHP} )", ConsoleColor.Red);
+            }
+            else
+            {
+                Message.WriteLine($"{character.Name, 59} ( {character.CurrentHP}/{character.MaxHP} )", ConsoleColor.Red);
+            }
+        }
+
+        Message.WriteLine(("").PadRight(68, '='), ConsoleColor.White);
+    }
+
     public bool Start()
     {
         Message.WriteLine("A new battle begins! Prepare yourself...", ConsoleColor.White);
@@ -30,12 +63,14 @@
         {
             for (int i = 0; i < Heroes.Characters.Count; i++)
             {
+                Status(Heroes.Characters[i]);
                 Heroes.Characters[i].PlayerAction(Heroes.Player, Monsters);
                 EnemyHealthCheck(Monsters);
             }
 
             for (int i = 0; i < Monsters.Characters.Count; i++)
             {
+                Status(Monsters.Characters[i]);
                 Monsters.Characters[i].PlayerAction(Monsters.Player, Heroes);
                 EnemyHealthCheck(Heroes);
             }
